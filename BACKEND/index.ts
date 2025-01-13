74b36e21adc0c284943068b2b7ff0197297b4efa
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-01-13 17:49:57 +0800

    asdfajosdjkfk

--- BACKEND/index.ts
+++ BACKEND/index.ts
@@ -1,12 +1,28 @@
 import { configDotenv } from "dotenv";
 import express, { Express, Response, Request } from "express";
+const mongoose = require("mongoose");
 configDotenv();
-const URI = `mongodb+srv://myasglpzghoo:<${process.env.DB_PASSWORD}>@cluster0.imwim.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const URI = `mongodb+srv://myasglpzghoo:<${process.env.DB_PASSWORD}>@cluster0.imwim.mongodb.net/food-delivery?retryWrites=true&w=majority&appName=Cluster0`;
 const app: Express = express();
 app.use(express.json());
-
+const fetchDb = async () => {
+  if (!URI) {
+    return;
+  }
+  try {
+    await mongoose.connect(URI);
+  } catch (err) {
+    console.error("error lol", err);
+  }
+};
+fetchDb();
+const schema = new mongoose.Schema({
+  categoryName: String,
+});
+const model = mongoose.model("foodCategory", schema, "food-category");
 app.get("/", (Req: Request, res: Response) => {
-  res.json("lol");
+  const getTheItems = model.find();
+  res.json(getTheItems);
 });
 
 const port = 5000;
